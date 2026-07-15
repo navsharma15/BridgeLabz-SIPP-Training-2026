20e34637ed44db6404418e3e25ad7d14c6b4913e
Nav Sharma <navdhruv12@gmail.com>
2026-07-15 22:58:21 +0530

    [NAV SHARMA] ADDED Stack Solutions

---
+++ DSA/Stack/BalancedBrackets/BalancedBrackets.java
@@ -0,0 +1,35 @@
+package BalancedBrackets;
+import java.util.*;
+
+public class BalancedBrackets {
+    public static boolean isValidConfig(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+
+                if (stack.isEmpty() || stack.pop() != map.get(ch)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isValidConfig("()[]{}"));
+        System.out.println(isValidConfig("([{}])"));
+        System.out.println(isValidConfig("(]"));
+    }
+}

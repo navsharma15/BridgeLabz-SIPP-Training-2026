6280ed6857aa993b328e54fbfcddb9f44dbb731c
Nav Sharma <navdhruv12@gmail.com>
2026-07-17 10:14:16 +0530

    [NAV SHARMA] ADDED Hashing , BST and Tree Solutions

---
+++ DSA/BinarySearchTree/ValidateBST/Node.java
@@ -0,0 +1,11 @@
+package ValidateBST;
+
+public class Node {
+    int val;
+    Node left, right;
+
+    Node(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
---
+++ DSA/Tree/HeightofBinaryTree/Node.java
@@ -0,0 +1,11 @@
+package HeightofBinaryTree;
+
+public class Node {
+    int val;
+    Node left, right;
+
+    Node(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
